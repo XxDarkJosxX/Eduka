ec2e776e7a2d825327c87d0041a8919b115c880b
JoseM <maytadazajos@gmail.com>
2025-09-04 17:01:56 -0400

    firs

--- Views/Registros/registros.php
+++ Views/Registros/registros.php
@@ -31,16 +31,15 @@ headerprincipal($data);
             <div class="text-center">
                 <button class="btn btn-primary btncrear" type="submit">Crear Usuario</button>
             
-           
-
-                    <div class="g_id_signin"
-                        data-type="standard"
-                        data-shape="pill"
-                        data-theme="outline"
-                        data-text="signin_with"
-                        data-size="large"
-                        data-logo_alignment="left">
+                    <div id="g_id_onload"
+                        data-client_id="196182658810-gri5vaek708sgnukf8rov1ke9i7iu62d.apps.googleusercontent.com"
+                        data-context="signin"
+                        data-ux_mode="popup"
+                        data-callback="handleCredentialResponse"
+                        data-auto_prompt="false">
                     </div>
+
+               
 
                     <div id="status"></div>
               
