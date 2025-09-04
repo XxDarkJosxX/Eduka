8ed7736e245d02e417f6d30e450f817c2210e9f4
JoseM <maytadazajos@gmail.com>
2025-09-04 16:13:25 -0400

    firs

--- Views/Registros/registros.php
+++ Views/Registros/registros.php
@@ -16,10 +16,7 @@ headerprincipal($data);
                 <label class="form-label">Apellido</label>
                 <input id="txtapellido" name="txtapellido" type="text" class="form-control" placeholder="Apellido">
             </div>
-            <div class="form-group">
-                <label class="form-label">Telefono</label>
-                <input id="txttelefono" name="txttelefono" type="text" class="form-control" placeholder="Telefono">
-            </div>
+        
             <div class="form-group">
                 <label class="form-label">Correo</label>
                 <input id="txtcorreo" name="txtcorreo" type="email" class="form-control" placeholder="usuario@gmail.com">
