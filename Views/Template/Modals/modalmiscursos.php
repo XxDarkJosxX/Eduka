a7d7314460b4ade11dd8095d8788ee0cf616c679
JoseM <maytadazajos@gmail.com>
2025-09-04 19:31:35 -0400

    firs

--- Views/Template/Modals/modalmiscursos.php
+++ Views/Template/Modals/modalmiscursos.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="modalformcursos" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade" id="modalmiscursos" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header headerregister">
