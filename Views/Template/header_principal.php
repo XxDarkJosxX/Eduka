98feb876f6eb7aadd809fbb32c1bd698aad83088
JoseM <maytadazajos@gmail.com>
2025-09-04 16:42:07 -0400

    firs

--- Views/Template/header_principal.php
+++ Views/Template/header_principal.php
@@ -6,7 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <title>Educka</title>
-
+    <link rel="icon" type="image/png" href="<?= media() ?>/images/illustration/student/128/white.svg">
    <!-- Prevent the demo from appearing in search engines -->
    <meta name="robots" content="noindex">
 
